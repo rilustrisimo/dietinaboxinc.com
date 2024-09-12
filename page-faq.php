622a28af337685822c78f1f56a1e049e03523ebc
Rouie Ilustrisimo <ilustrisimo.rouie@outlook.com>
2024-09-12 09:12:14 +0800

    payment option

--- page-faq.php
+++ page-faq.php
@@ -175,7 +175,7 @@ get_header();
                         <div id="faq-10" class="collapse" data-parent="#accordionList">
                             <div class="card-body">
                                 <hr />
-                                <p>When choosing the Direct Online Payment Transfer option, you'll need to wire transfer payment using this QR Code below. <b>This QR code accepts payments from UnionBank, BDO, Gcash, Maya, PalawanPay, Robinsons Bank Corp., ShopeePay, BPI/BP|Family, Gpay Network, Metropolitan Bank and Trust Company, Rizal CommercialBanking Corp., Asia United Bank, Land Bank of The Philippines and Security Bank.</b></p><img src='https://www.dietinaboxinc.com/wp-content/uploads/2024/09/diab-qr.jpg'><p class='highlight'>Proof of payment has to be sent to <a href='mailto:chime@dietinaboxinc.com'>chime@dietinaboxinc.com</a></p>
+                                <p>When choosing the Direct Online Payment Transfer option, you'll need to wire transfer payment using this QR Code below. <b>This QR code accepts payments from UnionBank, BDO, Gcash, Maya, PalawanPay, Robinsons Bank Corp., ShopeePay, BPI/BP|Family, Gpay Network, Metropolitan Bank and Trust Company, Rizal CommercialBanking Corp., Asia United Bank, Land Bank of The Philippines and Security Bank.</b></p><img src='https://www.dietinaboxinc.com/wp-content/uploads/2024/09/diab-qr.jpg'><p class='highlight'>Proof of payment has to be sent to chime@dietinaboxinc.com</p>
                             </div>
                         </div>
                     </div>
